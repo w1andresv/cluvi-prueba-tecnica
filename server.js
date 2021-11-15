88fd2c08e21bb0ce3ca42ad1bc6daefddb9f193f
weymar <w1andresv@gmail.com>
2021-11-15 09:41:02 -0500

    ajustes para desplegar heroku

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
-//Install express server
-
-
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/cluvi-prueba-tecnica'));
+app.use(express.static(__dirname + '/dist/cluvi-prueba-tecnica'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/cluvi-prueba-tecnica/'}),
-);
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/cluvi-prueba-tecnica/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
-console.log('inicio servidor')
 app.listen(process.env.PORT || 8080);
-
